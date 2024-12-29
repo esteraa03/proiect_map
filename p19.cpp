28ddb8bdf0d1235ee806ee61b1829b6361ed229d
esteraa03 <123783603+esteraa03@users.noreply.github.com>
2024-12-29 21:04:25 +0200

    Add files via upload

---
+++ p19.cpp
@@ -0,0 +1,12 @@
+﻿/*Traversarea arborilor binari ordonați – Se dă un arbore binar ordonat care se 
+populează cu elemente aleatorii. Să se afișeze elementele în urmă traversării 
+arborelui în „pre-ordine”,  „in-ordine”, „post-ordine” și pe „nivel” (level-order 
+traversal).*/
+
+#include <iostream>
+
+int main()
+{
+
+	return 0;
+}
